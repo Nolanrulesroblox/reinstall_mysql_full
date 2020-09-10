25db5abc0cb7d449bcddbb92146399d47de1ef21
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2020-09-10 00:52:12 -0700

    beta

--- reinstallmysql.sh
+++ reinstallmysql.sh
@@ -1,1 +1,12 @@
-sudo rm -r /var/lib/mysql ; sudo rm -r /etc/mysql ; sudo apt-get remove --purge mysql-server mysql-client mysql-common -y; sudo apt-get autoremove ; sudo apt-get autoclean ; sudo apt-get install mysql-server ;
+#!/bin/bash
+sudo /etc/init.d/mysql stop
+#stops sql service
+sudo apt-get remove --purge mysql-server mysql-client mysql-common -y;
+#removes mysql
+ sudo rm -r /var/lib/mysql ;
+  sudo rm -r /etc/mysql ;
+#removes anything to do with mysql
+  sudo apt-get autoremove ;
+ sudo apt-get autoclean ;
+#cleans out junk
+   sudo apt-get install mysql-server ;
